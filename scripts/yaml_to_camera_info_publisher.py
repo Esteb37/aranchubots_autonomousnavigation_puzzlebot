2f85a366afc044b6d02cc73451bf8af782fc42da
karencl <a01704254@itesm.mx>
2024-05-24 14:39:44 -0600

    minichallenge6

---
+++ scripts/yaml_to_camera_info_publisher.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import rospy
+import yaml
+from sensor_msgs.msg import CameraInfo
+
+def yaml_to_CameraInfo(yaml_fname):
+    # Load data from file
+    with open(yaml_fname, "r") as file_handle:
+        calib_data = yaml.load(file_handle)
+
+    # Parse
+    camera_info_msg = CameraInfo()
+    camera_info_msg.width = calib_data["image_width"]
+    camera_info_msg.height = calib_data["image_height"]
+    camera_info_msg.K = calib_data["camera_matrix"]["data"]
+    camera_info_msg.D = calib_data["distortion_coefficients"]["data"]
+    camera_info_msg.R = calib_data["rectification_matrix"]["data"]
+    camera_info_msg.P = calib_data["projection_matrix"]["data"]
+    camera_info_msg.distortion_model = calib_data["distortion_model"]
+    return camera_info_msg
+
+if __name__ == "__main__":
+    filename = "/home/karenc/catkin_ws/src/minichallenge6/include/ost.yaml"
+
+    # Parse yaml file
+    camera_info_msg = yaml_to_CameraInfo(filename)
+
+    # Initialize publisher node
+    rospy.init_node("camera_info_publisher", anonymous=True)
+    publisher = rospy.Publisher("camera_info", CameraInfo, queue_size=10)
+    rate = rospy.Rate(10)
+
+    # Run publisher
+    while not rospy.is_shutdown():
+        publisher.publish(camera_info_msg)
+        rate.sleep()
+
